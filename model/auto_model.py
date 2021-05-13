0332863f8276a098071c23dc095f91a971bf17f5
Sunliangtai <50163989+Sunliangtai@users.noreply.github.com>
2021-05-13 16:52:30 +0800

    OpenAIGPTForMultipleChoice Added

--- model/auto_model.py
+++ model/auto_model.py
@@ -1,7 +1,11 @@
 from transformers.models.auto.auto_factory import auto_class_factory
 from transformers.models.auto.modeling_auto import MODEL_FOR_MULTIPLE_CHOICE_MAPPING
 from transformers.models.auto.configuration_auto import OpenAIGPTConfig
+from model.multi_choice import OpenAIGPTForMultipleChoice
+
+mapping = MODEL_FOR_MULTIPLE_CHOICE_MAPPING
+mapping[OpenAIGPTConfig] = OpenAIGPTForMultipleChoice
 
 AutoModelForMultipleChoice = auto_class_factory(
-    "AutoModelForMultipleChoice", MODEL_FOR_MULTIPLE_CHOICE_MAPPING, head_doc="multiple choice"
+    "AutoModelForMultipleChoice", mapping, head_doc="multiple choice"
 )
--- model/multi_choice.py
+++ model/multi_choice.py
@@ -1,41 +1,85 @@
+"""PyTorch OpenAI GPT model."""
+
 import torch
 import torch.nn as nn
-from torch.nn import KLDivLoss, MSELoss
-import torch.nn.functional as F
+from torch.nn import CrossEntropyLoss
+from transformers.modeling_outputs import MultipleChoiceModelOutput
 
-from transformers.activations import ACT2FN
+from transformers.utils import logging
+from transformers.models.openai import OpenAIGPTPreTrainedModel, OpenAIGPTModel
 from transformers.file_utils import (
-    ModelOutput,
     add_code_sample_docstrings,
     add_start_docstrings,
     add_start_docstrings_to_model_forward,
-    replace_return_docstrings,
 )
-from transformers.modeling_outputs import (
-    BaseModelOutput,
-    BaseModelOutputWithPooling,
-    MaskedLMOutput,
-    MultipleChoiceModelOutput,
-    QuestionAnsweringModelOutput,
-    SequenceClassifierOutput,
-    TokenClassifierOutput,
-)
-from transformers.modeling_utils import (
-    PreTrainedModel,
-    apply_chunking_to_forward,
-    find_pruneable_heads_and_indices,
-    prune_linear_layer,
-)
-import logging
-logger = logging.getLogger(__name__)
+from transformers.modeling_utils import SequenceSummary
+logger = logging.get_logger(__name__)
 
-_CHECKPOINT_FOR_DOC = "albert-base-v2"
-_CONFIG_FOR_DOC = "AlbertConfig"
-_TOKENIZER_FOR_DOC = "AlbertTokenizer"
+_CHECKPOINT_FOR_DOC = "openai-gpt"
+_CONFIG_FOR_DOC = "OpenAIGPTConfig"
+_TOKENIZER_FOR_DOC = "OpenAIGPTTokenizer"
+
+OPENAI_GPT_PRETRAINED_MODEL_ARCHIVE_LIST = [
+    "openai-gpt",
+    # See all OpenAI GPT models at https://huggingface.co/models?filter=openai-gpt
+]
 
 
-from transformers import AlbertPreTrainedModel, AlbertModel
-from transformers.models.albert.modeling_albert import ALBERT_START_DOCSTRING, ALBERT_INPUTS_DOCSTRING
+OPENAI_GPT_START_DOCSTRING = r"""
+    This model inherits from :class:`~transformers.PreTrainedModel`. Check the superclass documentation for the generic
+    methods the library implements for all its model (such as downloading or saving, resizing the input embeddings,
+    pruning heads etc.)
+    This model is also a PyTorch `torch.nn.Module <https://pytorch.org/docs/stable/nn.html#torch.nn.Module>`__
+    subclass. Use it as a regular PyTorch Module and refer to the PyTorch documentation for all matter related to
+    general usage and behavior.
+    Parameters:
+        config (:class:`~transformers.OpenAIGPTConfig`): Model configuration class with all the parameters of the model.
+            Initializing with a config file does not load the weights associated with the model, only the
+            configuration. Check out the :meth:`~transformers.PreTrainedModel.from_pretrained` method to load the model
+            weights.
+"""
+
+
+OPENAI_GPT_INPUTS_DOCSTRING = r"""
+    Args:
+        input_ids (:obj:`torch.LongTensor` of shape :obj:`(batch_size, sequence_length)`):
+            Indices of input sequence tokens in the vocabulary.
+            Indices can be obtained using :class:`~transformers.OpenAIGPTTokenizer`. See
+            :meth:`transformers.PreTrainedTokenizer.encode` and :meth:`transformers.PreTrainedTokenizer.__call__` for
+            details.
+            `What are input IDs? <../glossary.html#input-ids>`__
+        attention_mask (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`):
+            Mask to avoid performing attention on padding token indices. Mask values selected in ``[0, 1]``:
+            - 1 for tokens that are **not masked**,
+            - 0 for tokens that are **masked**.
+            `What are attention masks? <../glossary.html#attention-mask>`__
+        token_type_ids (:obj:`torch.LongTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`):
+            Segment token indices to indicate first and second portions of the inputs. Indices are selected in ``[0,
+            1]``:
+            - 0 corresponds to a `sentence A` token,
+            - 1 corresponds to a `sentence B` token.
+            `What are token type IDs? <../glossary.html#token-type-ids>`_
+        position_ids (:obj:`torch.LongTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`):
+            Indices of positions of each input sequence tokens in the position embeddings. Selected in the range ``[0,
+            config.max_position_embeddings - 1]``.
+            `What are position IDs? <../glossary.html#position-ids>`__
+        head_mask (:obj:`torch.FloatTensor` of shape :obj:`(num_heads,)` or :obj:`(num_layers, num_heads)`, `optional`):
+            Mask to nullify selected heads of the self-attention modules. Mask values selected in ``[0, 1]``:
+            - 1 indicates the head is **not masked**,
+            - 0 indicates the head is **masked**.
+        inputs_embeds (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, sequence_length, hidden_size)`, `optional`):
+            Optionally, instead of passing :obj:`input_ids` you can choose to directly pass an embedded representation.
+            This is useful if you want more control over how to convert :obj:`input_ids` indices into associated
+            vectors than the model's internal embedding lookup matrix.
+        output_attentions (:obj:`bool`, `optional`):
+            Whether or not to return the attentions tensors of all attention layers. See ``attentions`` under returned
+            tensors for more detail.
+        output_hidden_states (:obj:`bool`, `optional`):
+            Whether or not to return the hidden states of all layers. See ``hidden_states`` under returned tensors for
+            more detail.
+        return_dict (:obj:`bool`, `optional`):
+            Whether or not to return a :class:`~transformers.file_utils.ModelOutput` instead of a plain tuple.
+"""
 
 
 @add_start_docstrings(
@@ -50,12 +94,17 @@ from transformers.models.albert.modeling_albert import ALBERT_START_DOCSTRING, A
     """,
     OPENAI_GPT_START_DOCSTRING,
 )
-class OpenAIGPTForMultiChoice(OpenAIGPTPreTrainedModel):
+class OpenAIGPTForMultipleChoice(OpenAIGPTPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
+        config.num_labels = 1
+        # config.summary_type = "last"
+        self.config = config
         self.num_labels = config.num_labels
         self.transformer = OpenAIGPTModel(config)
-        self.score = nn.Linear(config.n_embd, self.num_labels, bias=False)
+        # self.multiple_choice_head = SequenceSummary(config)
+        self.dropout = nn.Dropout(0.1)
+        self.score = nn.Linear(config.n_embd, self.num_labels)
 
         self.init_weights()
 
@@ -63,7 +112,7 @@ class OpenAIGPTForMultiChoice(OpenAIGPTPreTrainedModel):
     @add_code_sample_docstrings(
         tokenizer_class=_TOKENIZER_FOR_DOC,
         checkpoint=_CHECKPOINT_FOR_DOC,
-        output_type=SequenceClassifierOutput,
+        output_type=MultipleChoiceModelOutput,
         config_class=_CONFIG_FOR_DOC,
     )
     def forward(
@@ -86,6 +135,16 @@ class OpenAIGPTForMultiChoice(OpenAIGPTPreTrainedModel):
             If :obj:`config.num_labels > 1` a classification loss is computed (Cross-Entropy).
         """
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        batch_size = input_ids.size(0)
+        input_ids = input_ids.view(-1, input_ids.size(-1)) if input_ids is not None else None
+        attention_mask = attention_mask.view(-1, attention_mask.size(-1)) if attention_mask is not None else None
+        token_type_ids = token_type_ids.view(-1, token_type_ids.size(-1)) if token_type_ids is not None else None
+        position_ids = position_ids.view(-1, position_ids.size(-1)) if position_ids is not None else None
+        inputs_embeds = (
+            inputs_embeds.view(-1, inputs_embeds.size(-2), inputs_embeds.size(-1))
+            if inputs_embeds is not None
+            else None
+        )
 
         transformer_outputs = self.transformer(
             input_ids,
@@ -99,48 +158,41 @@ class OpenAIGPTForMultiChoice(OpenAIGPTPreTrainedModel):
             return_dict=return_dict,
         )
 
+        # mc_token_ids = torch.tensor([input_ids.size(-1) - 1, input_ids.size(-1) - 1], device=input_ids.device).unsqueeze(0)
+
         hidden_states = transformer_outputs[0]
+        hidden_states = self.dropout(hidden_states)
         logits = self.score(hidden_states)
+        # mc_logits = self.multiple_choice_head(hidden_states).view(batch_size, -1)
 
-        if input_ids is not None:
-            batch_size, sequence_length = input_ids.shape[:2]
-        else:
-            batch_size, sequence_length = inputs_embeds.shape[:2]
-
-        assert (
-            self.config.pad_token_id is not None or batch_size == 1
-        ), "Cannot handle batch sizes > 1 if no padding token is defined."
         if self.config.pad_token_id is None:
             sequence_lengths = -1
         else:
             if input_ids is not None:
                 sequence_lengths = torch.ne(input_ids, self.config.pad_token_id).sum(-1) - 1
+                sequence_lengths = sequence_lengths.view(-1)
             else:
                 sequence_lengths = -1
                 logger.warning(
                     f"{self.__class__.__name__} will not detect padding tokens in `inputs_embeds`. Results may be "
-                    f"unexpected if using padding tokens in conjuction with `inputs_embeds.`"
+                    f"unexpected if using padding tokens in conjunction with `inputs_embeds.`"
                 )
 
-        pooled_logits = logits[range(batch_size), sequence_lengths]
+        mc_logits = logits[range(input_ids.size(0)), sequence_lengths]
+        mc_logits = mc_logits.view(batch_size, -1)
 
         loss = None
         if labels is not None:
-            if self.num_labels == 1:
-                #  We are doing regression
-                loss_fct = MSELoss()
-                loss = loss_fct(pooled_logits.view(-1), labels.to(self.dtype).view(-1))
-            else:
-                loss_fct = CrossEntropyLoss()
-                loss = loss_fct(pooled_logits.view(-1, self.num_labels), labels.view(-1))
+            loss_fct = CrossEntropyLoss()
+            loss = loss_fct(mc_logits, labels.view(-1))
 
         if not return_dict:
-            output = (pooled_logits,) + transformer_outputs[1:]
+            output = (mc_logits,) + transformer_outputs[1:]
             return ((loss,) + output) if loss is not None else output
 
-        return SequenceClassifierOutput(
+        return MultipleChoiceModelOutput(
             loss=loss,
-            logits=pooled_logits,
+            logits=mc_logits,
             hidden_states=transformer_outputs.hidden_states,
             attentions=transformer_outputs.attentions,
         )
