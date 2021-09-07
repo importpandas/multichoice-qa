3022120d9d1f29dbc8f8be0449d466cced88c331
Hongshen Xu <1505746949@qq.com>
2021-09-07 16:00:34 +0800

    fixed the bug

--- utils/hyperparam.py
+++ utils/hyperparam.py
@@ -34,12 +34,18 @@ def hyperparam_path_for_two_stage_evidence_selector(model_args, data_args, train
     exp_name += f'__max_evi_seq_len_{data_args.max_evidence_seq_length}'
     exp_name += f'__max_seq_len_{data_args.max_seq_length}'
     if training_args.train_extensive_evidence_selector:
-        exp_name += f'__select_epochs_{training_args.num_train_selector_epochs}'
+        if hasattr(training_args, "num_train_selector_epochs"):
+            exp_name += f'__select_epochs_{training_args.num_train_selector_epochs}'
+        else:
+            exp_name += f'__select_epochs_{training_args.num_train_epochs}'
         exp_name += f'__evi_sam_num_{data_args.evidence_sampling_num}'
     if training_args.train_intensive_evidence_selector:
         if not training_args.train_extensive_evidence_selector:
             exp_name += f'__evi_sam_num_{data_args.evidence_sampling_num}'
-        exp_name += f'__veri_epochs_{training_args.num_train_verifier_epochs}'
+        if hasattr(training_args, "num_train_verifier_epochs"):
+            exp_name += f'__veri_epochs_{training_args.num_train_verifier_epochs}'
+        else:
+            exp_name += f'__veri_epochs_{training_args.num_train_epochs}'
         exp_name += f'__ise_evi_len_{data_args.intensive_evidence_len}'
         exp_name += f'__train_ise_with_opt_{data_args.train_intensive_selector_with_option}'
         exp_name += f'__ise_with_no_overlap_evi_{data_args.train_intensive_selector_with_non_overlapping_evidence}'
