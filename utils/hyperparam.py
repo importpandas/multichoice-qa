a6e379a7f990c68ba0200bc26f7b5264e1cdc729
Hongshen Xu <1505746949@qq.com>
2022-03-12 02:28:10 +0800

    fixed the bug

--- utils/hyperparam.py
+++ utils/hyperparam.py
@@ -58,7 +58,7 @@ def hyperparam_path_for_two_stage_evidence_selector(model_args, data_args, train
             exp_name += f'__init_veri_from_reader_{model_args.initialize_verifier_from_reader}'
         if data_args.train_verifier_with_sample_weighting:
             exp_name += f'__weighting_temperature_{data_args.weighting_temperature}'
-            exp_name += f'__weighting_temperature_{data_args.weighting_temperature}'
+            exp_name += f'__weighting_method_{data_args.weighting_method}'
     # try:
     #     if training_args.train_answer_verifier:
     #         exp_name += f'__veri_type_{model_args.verifier_type}'
